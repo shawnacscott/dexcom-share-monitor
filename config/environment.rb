6f1af89bf9c2ab525b4c096b589a5650d3d54fee
Shawna C. Scott <shawna.scott@stitchfix.com>
2019-12-01 01:13:02 -0800

    Set up Fittings for environment variable management

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load ENV variables
+Setting.load(
+  path:  "#{Rails.root}/config/settings",
+  files: ["default.yml", "environments/#{Rails.env}.yml"]
+)
+
 # Initialize the Rails application.
 Rails.application.initialize!
